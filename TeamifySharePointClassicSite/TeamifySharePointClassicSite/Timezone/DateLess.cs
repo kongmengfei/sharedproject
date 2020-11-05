1af1c81e6198c83d45eb9dab67b726111cfaa284
kongmengfei <57654821+kongmengfei@users.noreply.github.com>
2020-11-05 13:07:12 +0800

    20-11-5
    13:07

---
+++ TeamifySharePointClassicSite/TeamifySharePointClassicSite/Timezone/DateLess.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.SharePoint.Client;
+using OfficeDevPnP.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamifySharePointClassicSite
+{
+    class DateLess
+    {
+        static void Main(string[] args)
+        {
+            var siteurl = "https://isvdevchat.sharepoint.com/sites/sbdev";
+            var pwd = "dlmm=920625m";
+            var username = "admin@ISVDevChat.onmicrosoft.com";
+
+            var authManager = new AuthenticationManager();
+            ClientContext context = authManager.GetSharePointOnlineAuthenticatedContextTenant(siteurl, username, pwd);
+
+            // timezone
+            var spTimeZone = context.Web.RegionalSettings.TimeZone;        
+            List mylist = context.Web.Lists.GetByTitle("kkkk");
+            ListItem targetitems = mylist.GetItemById(17);
+
+            context.Load(spTimeZone);              
+            context.Load(targetitems);         
+            context.ExecuteQuery();
+
+            Console.WriteLine(spTimeZone.Description);
+            Console.WriteLine(targetitems["mStartDate"] is null);
+
+            var orginaldate = Convert.ToDateTime(targetitems["mStartDate"]);
+
+            string strStartdate = orginaldate.ToLocalTime().ToString();
+            Console.WriteLine(strStartdate);
+
+            ClientResult<DateTime> cr = spTimeZone.UTCToLocalTime(orginaldate);
+            context.ExecuteQuery();
+            Console.WriteLine(cr.Value);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TeamifySharePointClassicSite/TeamifySharePointClassicSite/Timezone/John1105John.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.SharePoint.Client;
+using OfficeDevPnP.Core;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamifySharePointClassicSite.Timezone
+{
+    class John1105John
+    {
+        static void Main(string[] args)
+        {
+            var siteurl = "https://isvdevchat.sharepoint.com/sites/sbdev";
+            var pwd = "dlmm=920625m";
+            var username = "admin@ISVDevChat.onmicrosoft.com";
+
+            var authManager = new AuthenticationManager();
+            ClientContext context = authManager.GetSharePointOnlineAuthenticatedContextTenant(siteurl, username, pwd);
+
+            // timezone
+            var spTimeZone = context.Web.RegionalSettings.TimeZone;            
+
+            //context.Load(spTimeZone);            
+            //context.ExecuteQuery();
+
+            //Console.WriteLine(spTimeZone.Description);
+
+
+            var orginaldate = DateTime.ParseExact("14/10/2020 13:14:11","dd/MM/yyyy HH:mm:ss", CultureInfo.InvariantCulture);
+
+            
+            //Console.WriteLine(orginaldate);
+
+            ClientResult<DateTime> cr = spTimeZone.UTCToLocalTime(orginaldate);
+            context.ExecuteQuery();
+            Console.WriteLine(cr.Value);
+
+            Console.ReadKey();
+        }
+    }
+}
