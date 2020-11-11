2195dc5b0cf6bb511ef07caa0c76f7be3a161d52
kongmengfei <57654821+kongmengfei@users.noreply.github.com>
2020-11-11 17:28:29 +0800

    20-11-11
    17:28

---
+++ SharePointAddInprovider/SharePointAddInproviderWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SharePointAddInproviderWeb.Global" Language="C#" %>
---
+++ SharePointAddInprovider/SharePointAddInproviderWeb/Pages/Default.aspx.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SharePointAddInproviderWeb
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        
+
+    }
+}
